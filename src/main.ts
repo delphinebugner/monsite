355adf3bfbac31212441d8d787d5bcc69ecd9fba
dbugner <delphine.bugner@artelys.com>
2020-07-26 14:07:00 +0200

    Back : config files pour heroku

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-declare const module: any;
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
-
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+  app.enableCors();
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
