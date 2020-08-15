504bdc52883fef1eb8283721af3a94db289b1bff
dbugner <delphine.bugner@artelys.com>
2020-08-15 19:24:14 +0200

    Front : révision routing pour la page principale (suite)

--- mon-site-frontend/src/MonSite.tsx
+++ mon-site-frontend/src/MonSite.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Switch, Route, useHistory, Redirect} from 'react-router-dom';
+import {Switch, Route, useHistory } from 'react-router-dom';
 import './MonSite.css';
 import AppBackend from "./backend/AppBackend";
 import Focus from "./components/Focus";
