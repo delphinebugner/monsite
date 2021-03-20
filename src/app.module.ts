adaa97d1ca88b2f9bd9672889239dc1d89de0e09
Delphine Bugner <delphineb@bam.tech>
2021-03-20 12:29:34 +0100

    config (typeorm) install typerom

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
@@ -10,6 +11,19 @@ import { CloudinaryModule } from './cloudinary/cloudinary.module';
     ConfigModule.forRoot({
       envFilePath: '.development.env',
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        host: process.env.DATABASE_URL,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: process.env.NODE_ENV !== 'production',
+        synchronize: process.env.NODE_ENV !== 'production',
+      }),
     }),
   ],
   controllers: [AppController],
